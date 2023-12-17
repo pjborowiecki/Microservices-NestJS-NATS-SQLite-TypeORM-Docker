d2ac93c5f8804bce8593392d7ab3d1b1d9145da9
P J Borowiecki <piotr.borowiecki@yahoo.com>
2023-12-17 19:29:04 +0100

    Define the createUser method

--- microservices/users/src/users/users.controller.ts
+++ microservices/users/src/users/users.controller.ts
@@ -2,13 +2,15 @@ import { Controller } from '@nestjs/common';
 import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 
 import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller()
 export class UsersMicroserviceController {
+  constructor(private usersService: UsersService) {}
+
   @MessagePattern({ cmd: 'createUser' })
   createUser(@Payload() data: CreateUserDto) {
-    console.log(data);
-    return { msg: 'success' };
+    return this.usersService.createUser(data);
   }
 
   @EventPattern('paymentCreated')
--- microservices/users/src/users/users.service.ts
+++ microservices/users/src/users/users.service.ts
@@ -4,9 +4,16 @@ import { Injectable } from '@nestjs/common';
 
 import { User } from 'src/typeorm/entities/User';
 
+import { CreateUserDto } from 'src/users/dtos/CreateUser.dto';
+
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private usersRepository: Repository<User>,
   ) {}
+
+  createUser(createUserDto: CreateUserDto) {
+    const newUser = this.usersRepository.create(createUserDto);
+    return this.usersRepository.save(newUser);
+  }
 }
